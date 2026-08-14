bd14595f5fbc4fa0a1b43ff266b53fd76ac6f0c0
Alfred Whitman <againsturb123@gmail.com>
2026-08-14 10:21:07 +0800

    Fix macOS Linker Issue

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("macos") {
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,/Library/Frameworks/GStreamer.framework/Versions/1.0/lib"
+        );
+    }
+
     tauri_build::build()
 }
